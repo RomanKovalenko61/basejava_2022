a6df8bae1d4c97cd7f23a06027d82def91245458
RomanKovalenko61 <astek14@mail.ru>
2022-12-21 16:53:52 +0300

    Lesson_03: implements interface Storage

--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -7,8 +7,10 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
+public class ArrayStorage implements Storage {
+
     private static final int STORAGE_LIMIT = 10_000;
+
     private final Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size;
 
